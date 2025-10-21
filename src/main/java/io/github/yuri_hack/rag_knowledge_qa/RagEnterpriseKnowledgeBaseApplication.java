624dc930356462497a50971a6f06f21c122ae31c
Mac <mac@Mac.bbrouter>
2025-10-21 14:11:57 +0800

    ADD:Initial commit: Spring Boot RAG project structure

---
+++ src/main/java/io/github/yuri_hack/rag_knowledge_qa/RagEnterpriseKnowledgeBaseApplication.java
@@ -0,0 +1,13 @@
+package io.github.yuri_hack.rag_knowledge_qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagEnterpriseKnowledgeBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagEnterpriseKnowledgeBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/yuri_hack/rag_knowledge_qa/RagEnterpriseKnowledgeBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yuri_hack.rag_knowledge_qa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagEnterpriseKnowledgeBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
