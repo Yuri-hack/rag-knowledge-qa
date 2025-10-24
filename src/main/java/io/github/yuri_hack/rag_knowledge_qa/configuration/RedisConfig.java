e39e30eda209c0d46a86d971fe9a981a303d0baa
Yuanqian Xiang <15690863316@163.com>
2025-10-24 16:42:15 +0800

    ADD:Redis Configuration

---
+++ src/main/java/io/github/yuri_hack/rag_knowledge_qa/configuration/RedisConfig.java
@@ -0,0 +1,36 @@
+package io.github.yuri_hack.rag_knowledge_qa.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    /**
+     * 自定义RedisTemplate，配置序列化方式
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringRedisSerializer);
+        template.setHashKeySerializer(stringRedisSerializer);
+
+        // 使用GenericJackson2JsonRedisSerializer来序列化和反序列化redis的value值
+        GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer = 
+            new GenericJackson2JsonRedisSerializer();
+        
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        
+        template.afterPropertiesSet();
+        return template;
+    }
+}
