05c259cf3ac01b7e29b33edde5c2891cb533026b
Yuanqian Xiang <15690863316@163.com>
2025-11-05 16:28:00 +0800

    MOD:milvus collection config field

--- src/main/java/io/github/yuri_hack/rag_knowledge_qa/config/MilvusConfig.java
+++ src/main/java/io/github/yuri_hack/rag_knowledge_qa/config/MilvusConfig.java
@@ -1,11 +1,15 @@
-package io.github.yuri_hack.rag_knowledge_qa.configuration;
+package io.github.yuri_hack.rag_knowledge_qa.config;
 
 import io.milvus.client.MilvusServiceClient;
 import io.milvus.param.ConnectParam;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
+@Data
 @Configuration
 public class MilvusConfig {
     
@@ -14,6 +18,18 @@ public class MilvusConfig {
     
     @Value("${milvus.port:19530}")
     private Integer port;
+
+    @Value("${milvus.collection.name:knowledge_base}")
+    private String collectionName;
+
+    @Value("${milvus.collection.vector-dimension:1024}")
+    private Integer vectorDimension;
+
+    @Value("${milvus.collection.partitions:default}")
+    private List<String> partitions;
+
+    @Value("${milvus.collection.recreate:false}")
+    private boolean recreate;
     
     @Bean
     public MilvusServiceClient milvusServiceClient() {
